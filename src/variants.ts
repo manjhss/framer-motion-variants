1bee746f84db22926d8641070d66734292028220
Surendra <manjhisurendra08@gmail.com>
2024-07-03 00:42:54 +0530

    Added variants

--- src/variants.ts
+++ src/variants.ts
@@ -7,3 +7,35 @@ export const fadeIn = {
 		transition: { duration: 1 },
 	},
 };
+
+export const parentFadeIn = {
+	initial: {
+		opacity: 0,
+	},
+	animate: {
+		opacity: 1,
+		transition: {
+			duration: 1,
+			when: "beforeChildren",
+			staggerChildren: 0.2,
+		},
+	},
+};
+
+export const childLeftFadeIn = {
+	initial: {
+		x: -20,
+		opacity: 0,
+	},
+	animate: {
+		x: 0,
+		opacity: 1,
+		transition: { type: "spring" },
+	},
+};
+
+export const hoverScaleUp = {
+	hover: {
+		scale: 1.1,
+	},
+};
