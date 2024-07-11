f93484a03d73902f211be6fb2da4f1e8396bfebf
Surendra <manjhisurendra08@gmail.com>
2024-07-11 15:31:03 +0530

    Added fadeIn and scaleUp variants

---
+++ src/animation.ts
@@ -0,0 +1,13 @@
+export const bounce = {
+  initial: {
+    y: 0,
+  },
+  animate: {
+    y: 10,
+    yoyo: Infinity,
+
+    translate: {
+      duration: 1,
+    },
+  },
+};
--- src/variants.ts
+++ src/variants.ts
@@ -1,41 +1,197 @@
-export const fadeIn = {
-	initial: {
-		opacity: 0,
-	},
-	animate: {
-		opacity: 1,
-		transition: { duration: 1 },
-	},
-};
+// PARENT
 
 export const parentFadeIn = {
-	initial: {
-		opacity: 0,
-	},
-	animate: {
-		opacity: 1,
-		transition: {
-			duration: 1,
-			when: "beforeChildren",
-			staggerChildren: 0.2,
-		},
-	},
+  initial: {
+    opacity: 0,
+  },
+  animate: {
+    opacity: 1,
+    transition: {
+      when: "beforeChildren",
+      staggerChildren: 0.6,
+      duration: 0.6,
+    },
+  },
 };
 
-export const leftFadeIn = {
-	initial: {
-		x: -20,
-		opacity: 0,
-	},
-	animate: {
-		x: 0,
-		opacity: 1,
-		transition: { type: "spring" },
-	},
+// CHILDREN
+
+// fade
+export const fadeIn = {
+  initial: {
+    opacity: 0,
+  },
+  animate: {
+    opacity: 1,
+    transition: { duration: 1 },
+  },
 };
 
-export const hoverScaleUp = {
-	hover: {
-		scale: 1.1,
-	},
+export const fadeInTop = {
+  initial: {
+    opacity: 0,
+    y: 24,
+  },
+  animate: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 1,
+    },
+  },
+};
+
+export const fadeInBottom = {
+  initial: {
+    opacity: 0,
+    y: -24,
+  },
+  animate: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 1,
+    },
+  },
+};
+
+export const fadeInLeft = {
+  initial: {
+    opacity: 0,
+    x: 24,
+  },
+  animate: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 1,
+    },
+  },
+};
+
+export const fadeInRight = {
+  initial: {
+    opacity: 0,
+    x: -24,
+  },
+  animate: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 1,
+    },
+  },
+};
+
+export const fadeInLeftRight = {
+  left: {
+    opacity: 0,
+    x: -24,
+  },
+  right: {
+    opacity: 0,
+    x: 24,
+  },
+  animate: {
+    opacity: 1,
+    x: 0,
+    transition: { duration: 1 },
+  },
+};
+
+// scale
+export const scaleUp = {
+  initial: {
+    opacity: 0,
+    scale: 0.6,
+  },
+  animate: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration: 1,
+    },
+  },
+};
+
+export const scaleUpTop = {
+  initial: {
+    opacity: 0,
+    scale: 0.6,
+    y: 60,
+  },
+  animate: {
+    opacity: 1,
+    scale: 1,
+    y: 0,
+    transition: {
+      duration: 1,
+    },
+  },
+};
+
+export const scaleUpBottom = {
+  initial: {
+    opacity: 0,
+    scale: 0.6,
+    y: -60,
+  },
+  animate: {
+    opacity: 1,
+    scale: 1,
+    y: 0,
+    transition: {
+      duration: 1,
+    },
+  },
+};
+
+export const scaleUpLeft = {
+  initial: {
+    opacity: 0,
+    scale: 0.6,
+    x: 60,
+  },
+  animate: {
+    opacity: 1,
+    scale: 1,
+    x: 0,
+    transition: {
+      duration: 1,
+    },
+  },
+};
+
+export const scaleUpRight = {
+  initial: {
+    opacity: 0,
+    scale: 0.6,
+    x: -60,
+  },
+  animate: {
+    opacity: 1,
+    scale: 1,
+    x: 0,
+    transition: {
+      duration: 1,
+    },
+  },
+};
+
+export const scaleUpHero = {
+  initial: {
+    opacity: 0,
+    scale: 0.6,
+    x: 60,
+    y: 60,
+  },
+  animate: {
+    opacity: 1,
+    scale: 1,
+    x: 0,
+    y: 0,
+    transition: {
+      duration: 1,
+    },
+  },
 };
